f6b0f9a0a14789ab289445067a4dd54bbb06b32f
Nicolas Lawson <nicolas.laws@gmail.com>
2026-05-10 12:58:17 -0400

    Made button to increment gold.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
+ "use client";
+
+ import { useState } from "react";
+
 export default function Home()
 {
-  const gold: number = 0;
+  const initialGold: number = 0;
+  const goldState: [number, (value: number) => void] = useState<number>(initialGold);
+  const setGold: (value: number) => void = goldState[1];
+  const goldIncrementValue: number = 12;
 
-  const content: React.ReactElement =
+  const incrementGoldByValue: () => void = () =>
+  {
+    const nextGold: number = goldState[0] + goldIncrementValue;
+    setGold(nextGold);
+  };
+
+  const showGoldComponent: React.ReactElement =
+  (
+    <div>
+      Gold: {goldState[0]}
+    </div>
+  );
+
+  const incrementGoldButton: React.ReactElement =
+  (
+    <
+      button
+      onClick={incrementGoldByValue}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    >
+      Increment gold by {goldIncrementValue}!
+    </button>
+  );
+
+  const pageComponent: React.ReactElement =
   (
     <main>
-      <div>
-        Gold: {gold}
-      </div>
+      {showGoldComponent}
+      {incrementGoldButton}
     </main>
   );
 
-  return content;
+  return pageComponent;
 }
