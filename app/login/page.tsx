5bf8c285ea94b677709df1a878e8cb6c6780967f
Nicolas Lawson <nicolas.laws@gmail.com>
2026-05-16 12:57:43 -0400

    Fixed login/register blank screen.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -44,14 +44,14 @@ export default function LoginPage()
 				placeholder="Username"
 				value={usernameState[0]}
 				onChange={(e) => setUsername(e.target.value)}
-				className="border px-2 py-1 rounded"
+				className="border border-gray-400 px-2 py-1 rounded bg-white text-black placeholder:text-gray-400"
 			/>
 			<input
 				type="password"
 				placeholder="Password"
 				value={passwordState[0]}
 				onChange={(e) => setPassword(e.target.value)}
-				className="border px-2 py-1 rounded"
+				className="border border-gray-400 px-2 py-1 rounded bg-white text-black placeholder:text-gray-400"
 			/>
 			{errorElement}
 			<button
