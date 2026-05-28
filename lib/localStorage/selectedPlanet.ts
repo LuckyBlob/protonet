c0a6c07296cdc31bc5ca0198d2c98a6555092fb2
Nicolas Lawson <nicolas.laws@gmail.com>
2026-05-28 14:24:18 -0400

    test push 2

--- lib/localStorage/selectedPlanet.ts
+++ lib/localStorage/selectedPlanet.ts
@@ -10,7 +10,6 @@ export function updateSelectedPlanetIdInStorage(newPlayerData: PlayerDataType.Pl
     const storedSelectedPlanetId: number | null = readStoredSelectedPlanetId();
     const resolvedSelectedPlanetId: number = getRelevantSelectedPlanetId(newPlayerData.fullPlanetDatas, storedSelectedPlanetId);
 
-    
     if (resolvedSelectedPlanetId !== storedSelectedPlanetId)
     {
         writeStoredSelectedPlanetId(resolvedSelectedPlanetId);
